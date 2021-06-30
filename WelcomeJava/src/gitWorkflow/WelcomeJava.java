d6e52c3a5c0955370f43c98c3b1a9e0f0837b317
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-30 22:06:12 +0530

    Welcome Java

--- WelcomeJava/src/gitWorkflow/WelcomeJava.java
+++ WelcomeJava/src/gitWorkflow/WelcomeJava.java
@@ -3,7 +3,7 @@ package gitWorkflow;
 public class WelcomeJava {
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome Java techei");
+		System.out.println("Welcome Java");
 	}
 
 }
