7e6f990f02a7abd33cb24ed7bc60dd4e2109e636
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-30 23:35:02 +0530

    chnages to msg

--- WelcomeJava/src/gitWorkflow/WelcomeJava.java
+++ WelcomeJava/src/gitWorkflow/WelcomeJava.java
@@ -3,12 +3,9 @@ package gitWorkflow;
 public class WelcomeJava {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Welcome Java");
-=======
 		System.out.println("Welcome Java techei");
 		System.out.println("Welcome programmers");
->>>>>>> e64fc938cbc9efa964af4fdbf76a1184da9b9c01
 	}
 
 }
